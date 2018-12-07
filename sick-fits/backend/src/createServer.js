0eb4148e0e5742faeca6b765a2acc4d763f31211
Milos Jovancevic <jovancevicmilos@gmail.com>
2018-12-07 18:04:07 +0100

    Yoga setup in progress

---
+++ sick-fits/backend/src/createServer.js
@@ -0,0 +1,12 @@
+const { GraphQLServer } = require('graphql-yoga');
+const Mutation = require('./resolvers/Mutation');
+const Query = require('./resolvers/Query');
+const db = require('./db');
+
+// Create the GraphQL Yoga Server
+
+function createServer() {
+
+}
+
+module.exports = createServer;
